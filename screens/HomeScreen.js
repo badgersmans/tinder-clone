90dec5bb244989449d0dad342be9893f3e15cf58
Shawn Law <shawnlaw@tuta.io>
2022-11-13 14:33:43 +0800

    7.12.18 done settin up react navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+import tw from 'twrnc';
+
+const HomeScreen = () => {
+  return (
+    <View style={tw`items-center justify-center flex-1`}>
+        <Text>pkpkpi098OIY(*!</Text>
+        <Button title='Click me'/>
+    </View>
+  )
+}
+
+export default HomeScreen
