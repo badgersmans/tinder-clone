3ed836e3ba9fcc4a0cbab1df5503b66882fd73bd
Shawn Law <shawnlaw@tuta.io>
2022-11-14 17:56:07 +0800

    8.22.05 done with login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,31 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button, SafeAreaView, ImageBackground, TouchableOpacity } from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import useAuth from '../hooks/useAuth'
+import { useNavigation } from '@react-navigation/native'
+import tw from 'twrnc'
 
 const LoginScreen = () => {
     const { loading, signInWithGoogle } = useAuth()
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, [])
   return (
-    <View>
-      <Text>{loading ? 'Loading...' : 'Login to the app'}</Text>
-      <Button title="Sign In With Google" onPress={signInWithGoogle}/>
+    <View style={tw`flex-1`}>
+        <ImageBackground
+            source={{uri: "https://tinder.com/static/tinder.png"}}
+            style={tw`flex-1`}
+        >
+            <TouchableOpacity 
+                onPress={signInWithGoogle} 
+                style={[tw`absolute bottom-28 w-52 bg-white p-4 rounded-2xl`, { marginHorizontal: '25%' }]}
+            >
+                <Text style={tw`text-center font-semibold`}>Sign in & get matched</Text>
+            </TouchableOpacity>
+        </ImageBackground>
     </View>
   )
 }
